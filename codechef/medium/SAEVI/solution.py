c47381444b512e899d07bfe1f9738afd7a835f1d
sriramkousik482-dot <sriramkousik482@gmail.com>
2026-09-07 19:12:38 +0530

    ✅ SAEVI | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-07

---
+++ codechef/medium/SAEVI/solution.py
@@ -0,0 +1,11 @@
+# cook your dish here
+n,k=map(int,input().split())
+arr=list(map(int,input().split()))
+sum=0
+for i in range(0,n,2):
+    if arr[i]>=2*k:
+        sum+=1
+    else:
+        print(0)
+    print(sum)
+        
