14d47a56755d16bc5a89a32527b56f02cc81a717
sriramkousik482-dot <sriramkousik482@gmail.com>
2026-09-14 21:23:28 +0530

    ✅ BSSEA | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/medium/BSSEA/solution.py
+++ codechef/medium/BSSEA/solution.py
@@ -1,1 +1,10 @@
 # cook your dish here
+n=int(input())
+arr=list(map(int,input().split()))
+median=arr[0]+arr[n-1]//2
+if median%2==0:
+    print(median)
+elif median%2=0:
+    print(min(arr))
+else:
+    print()
