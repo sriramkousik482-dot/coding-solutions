09f35a54a73647fce6e4ffa2bcb9d6674404ad18
sriramkousik482-dot <sriramkousik482@gmail.com>
2026-09-22 20:40:14 +0530

    ✅ Sort Colors | Medium | Python | Runtime: 5 ms (2.39%) | Memory: 19.4 MB | 2026-09-22

--- leetcode/medium/0075-sort-colors/solution.py
+++ leetcode/medium/0075-sort-colors/solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def sortColors(self, nums: list[int]) -> None:
         n=len(nums)
-        for j in range(n):
-            for i in range(1,n):
-                if nums[i]<nums[i-1]:
-                    nums[i],nums[i-1]=nums[i-1],nums[i]
+        for i in range(n):
+            for j in range(1,n):
+                if nums[j]<nums[j-1]:
+                    nums[j],nums[j-1]=nums[j-1],nums[j]
         print(*nums)
             
 
